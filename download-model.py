4a8a4d5c90bbcbaaa219c8376f93c2d79ac3c23e
Rafael Souza <rflpsz@gmail.com>
2023-08-29 23:37:30 -0300

    fixed download with 7B and 13B

--- download-model.py
+++ download-model.py
@@ -1,2 +1,3 @@
 from huggingface_hub import hf_hub_download
-hf_hub_download(repo_id="TheBloke/Llama-2-7B-Chat-GGML", filename="llama-2-7b-chat.ggmlv3.q8_0.bin")
+hf_hub_download(repo_id="TheBloke/Llama-2-7B-Chat-GGML", filename="llama-2-7b-chat.ggmlv3.q8_0.bin", output_path="/workspace/Llama-2-Open-Source-LLM-CPU-Inference/models/7B")
+# hf_hub_download(repo_id="TheBloke/Llama-2-13B-chat-GGML", filename="llama-2-13b-chat.ggmlv3.q8_0.bin", output_path="/workspace/Llama-2-Open-Source-LLM-CPU-Inference/models/13B")
